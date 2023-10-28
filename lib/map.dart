928dc4a5bb8667681997b5fb1023382a718c1830
yudai-kdix <yudai.homma222@gmail.com>
2023-10-28 16:58:31 +0900

    add 現在位置表示(途中)

--- lib/map.dart
+++ lib/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
@@ -10,8 +11,56 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
+  double? lati;
+  double? longi;
+  @override
+  void initState() {
+    super.initState();
+    location().then((position) {
+      setState(() {
+        lati = position.latitude;
+        longi = position.longitude;
+      });
+    });
+  }
 
-  final LatLng _center = const LatLng(34.6945454, 135.1952558);
+  Future<Position> location() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // 位置情報サービスが有効かどうかをテストします。
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // 位置情報サービスが有効でない場合、続行できません。
+      // 位置情報にアクセスし、ユーザーに対して
+      // 位置情報サービスを有効にするようアプリに要請する。
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      // ユーザーに位置情報を許可してもらうよう促す
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // 拒否された場合エラーを返す
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    // 永久に拒否されている場合のエラーを返す
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // ここまでたどり着くと、位置情報に対しての権限が許可されているということなので
+    // デバイスの位置情報を返す。
+    return await Geolocator.getCurrentPosition();
+  }
+
+  LatLng get _center => lati != null && longi != null
+      ? LatLng(lati!, longi!)
+      : const LatLng(34.6945454, 135.1952558);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -22,7 +71,7 @@ class _MapPageState extends State<MapPage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Maps Sample App'),
+          title: const Text('Map！！！！！！！！！'),
           backgroundColor: Color.fromARGB(255, 235, 159, 7),
         ),
         body: GoogleMap(
