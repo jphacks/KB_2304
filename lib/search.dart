55b0bfbad7d3650285dca5f7c5eb643283fe11e5
hamuhamu5hamu <fujisyo5090@au.com>
2023-10-28 13:16:32 +0900

    add
    検索ページ作成

--- lib/search.dart
+++ lib/search.dart
@@ -1,4 +1,123 @@
 import 'package:flutter/material.dart';
 
-class Search{
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _searchBoolean = false;
+  List<int> _searchIndexList = [];
+  List<String> _list = [
+    'English Textbook',
+    'Japanese Textbook',
+    'English Vocabulary',
+    'Japanese Vocabulary'
+  ];
+
+  Widget _searchTextField() {
+    return TextField(
+      onChanged: (String s) {
+        //追加
+        setState(() {
+          _searchIndexList = [];
+          for (int i = 0; i < _list.length; i++) {
+            if (_list[i].contains(s)) {
+              _searchIndexList.add(i);
+            }
+          }
+        });
+      },
+      autofocus: true, //TextFieldが表示されるときにフォーカスする（キーボードを表示する）
+      cursorColor: Colors.white, //カーソルの色
+      style: TextStyle(
+        //テキストのスタイル
+        color: Colors.white,
+        fontSize: 20,
+      ),
+      textInputAction: TextInputAction.search, //キーボードのアクションボタンを指定
+      decoration: InputDecoration(
+        //TextFiledのスタイル
+        enabledBorder: UnderlineInputBorder(
+            //デフォルトのTextFieldの枠線
+            borderSide: BorderSide(color: Colors.white)),
+        focusedBorder: UnderlineInputBorder(
+            //TextFieldにフォーカス時の枠線
+            borderSide: BorderSide(color: Colors.white)),
+        hintText: 'Search', //何も入力してないときに表示されるテキスト
+        hintStyle: TextStyle(
+          //hintTextのスタイル
+          color: Colors.white60,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+
+  Widget _searchListView() {
+    return ListView.builder(
+        itemCount: _searchIndexList.length, //編集
+        itemBuilder: (context, index) {
+          index = _searchIndexList[index]; //追加
+          return Card(child: ListTile(title: Text(_list[index])));
+        });
+  }
+
+  Widget _defaultListView() {
+    return ListView.builder(
+        itemCount: _list.length,
+        itemBuilder: (context, index) {
+          return Card(child: ListTile(title: Text(_list[index])));
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: !_searchBoolean ? Text(widget.title) : _searchTextField(),
+            actions: !_searchBoolean
+                ? [
+                    IconButton(
+                        icon: Icon(Icons.search),
+                        onPressed: () {
+                          setState(() {
+                            _searchBoolean = true;
+                            _searchIndexList = [];
+                          });
+                        })
+                  ]
+                : [
+                    IconButton(
+                        icon: Icon(Icons.clear),
+                        onPressed: () {
+                          setState(() {
+                            _searchBoolean = false;
+                          });
+                        })
+                  ]),
+        body: !_searchBoolean ? _defaultListView() : _searchListView());
+  }
 }
